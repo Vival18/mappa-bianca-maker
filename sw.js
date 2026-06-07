96fd354d397e26062b73ff4dcba5af51368e2f52
Vival18 <fabio.vivaldi97@gmail.com>
2026-06-07 17:04:08 +0200

    Fix: gestione errori

--- sw.js
+++ sw.js
@@ -1,45 +1,28 @@
-const CACHE_NAME = 'mappa-bianca-cache-v3';
+const CACHE_NAME = 'mb-cache-v1';
 const urlsToCache = [
   '/mappa-bianca-webapp/',
   '/mappa-bianca-webapp/index.html',
-  '/mappa-bianca-webapp/markers.json',
-  '/mappa-bianca-webapp/libs/leaflet.css',
-  '/mappa-bianca-webapp/libs/leaflet.js',
-  '/mappa-bianca-webapp/libs/MarkerCluster.css',
-  '/mappa-bianca-webapp/libs/MarkerCluster.Default.css',
-  '/mappa-bianca-webapp/libs/leaflet.markercluster.js'
+  '/mappa-bianca-webapp/markers.json'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Cache aperta');
-        return cache.addAll(urlsToCache);
-      })
-      .catch(err => console.error('Errore cache addAll:', err))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
   self.skipWaiting();
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cache => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
-      );
-    })
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+    ))
   );
   self.clients.claim();
 });
